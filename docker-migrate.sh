de6c4880c0290d49d859add09ea1106bfffc716f
jramseye <jramseye@redhat.com>
2015-07-22 17:41:41 -0400

    fixing issue 1 from github
    Signed-off-by: jramseye <jramseye@redhat.com>

--- docker-migrate.sh
+++ docker-migrate.sh
@@ -2,6 +2,10 @@
 # Here is a script to migrate docker images from one instance of docker to another
 
 if [ "$1" = "export" ]; then
+    if [ -z "$2" ]; then
+	echo "Please specify a directory and try again"
+	exit
+    fi
     if [ ! -d $2 ]; then
 	mkdir $2
     fi
@@ -15,13 +19,14 @@ if [ "$1" = "export" ]; then
 	mkdir $2/volumes
     fi
     for i in $( sudo docker images -q ); do
-	echo $i
+	echo "Saving image "$i
 	sudo docker save $i > $2/images/$i.tar
     done
-    sudo tar -zcvf $2/volumes/volumeData.tar.gz /var/lib/docker/volumes
+    sudo tar -zcvf $2/volumes/volumeData.tar.gz -C /var/lib/docker/volumes . > /dev/null
     if [ -d /var/lib/docker/vfs ]; then
-	sudo tar -zcvf $2/volumes/vfsData.tar.gz /var/lib/docker/vfs
+	sudo tar -zcvf $2/volumes/vfsData.tar.gz -C /var/lib/docker/vfs .
     fi
+    #Above we copy the contents of those folders, instead of the whole directory tree
     
 elif [ "$1" = "import" ]; then
     if [ ! -d "$2" ]; then
@@ -29,10 +34,13 @@ elif [ "$1" = "import" ]; then
 	exit
     fi
     for i in $( ls $2/images ); do
-	echo $i
+	echo "Loading image "$i
 	sudo docker load < $2/images/$i
     done
-    gunzip $2/volumes/volumeData.tar.gz
-    sudo mv $2/volumes/volumeData.tar /var/lib/docker
-    sudo tar xvf /var/lib/docker/volumeData.tar
+    sudo tar xzvf $2/volumes/volumeData.tar.gz
+    cp /var/lib/docker/volumes $2/volumes
+#    sudo mv $2/volumes/volumeData.tar /var/lib/docker
+#    sudo tar xvf /var/lib/docker/volumeData.tar
+else
+    echo "Please specify an option: 'import' or 'export' and try again"
 fi
