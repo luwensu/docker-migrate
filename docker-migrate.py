9f292eb44cc48cf71b3a05814cfbd03a392844f6
jramseye <jramseye@redhat.com>
2015-08-03 12:07:09 -0400

    fixed help message bug in code.  Refactored
    Signed-off-by: jramseye <jramseye@redhat.com>

--- docker-migrate.py
+++ docker-migrate.py
@@ -5,51 +5,57 @@ import sys
 import os
 import subprocess
 
-if sys.argv[1] == "export" :
-    if len(sys.argv) != 3:
-        sys.exit("Please specify a directory and try again")
-    if not os.path.isdir(sys.argv[2]):
-        os.mkdir(sys.argv[2])
-    if not os.path.isdir(sys.argv[2] + "/images"):
-        os.mkdir(sys.argv[2] + "/images")
-    if not os.path.isdir(sys.argv[2] + "/containers"):
-        os.mkdir(sys.argv[2] + "/containers")
-    if not os.path.isdir(sys.argv[2] + "/volumes"):
-        os.mkdir(sys.argv[2] + "/volumes")
-    # The following is some rather hacky list operations so that we can save images by name:tags instead of by ID
-    images = subprocess.check_output("sudo docker images", shell=True)
-    splitImages = images.split()[7:] # cut off the headers
-    names = []
-    tags = []
-    for i in range(0, len(splitImages)):
-        # only take the image and its tags
-        if (i % 8 == 0):
-            names.append(splitImages[i])
-            tags.append(splitImages[i+1])
-    for i in range(0, len(names)):
-        print("Saving image {0}:{1}".format(names[i], tags[i]))
-        subprocess.call("sudo docker save {0}:{1} > {2}/images/{0}-{1}.tar".format(names[i], tags[i], sys.argv[2]), shell=True)
-    subprocess.call("sudo tar -zcvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes . > /dev/null".format(sys.argv[2]), shell=True)
-    if os.path.isdir("/var/lib/docker/vfs"):
-        subprocess.call("sudo tar -zcvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs . > /dev/null".format(sys.argv[2]), shell=True)
+if len(sys.argv) <= 3:
+    if sys.argv[1] == "export":
+        if len(sys.argv) != 3:
+            sys.exit("Please specify a directory and try again")
+        if not os.path.isdir(sys.argv[2]):
+            os.mkdir(sys.argv[2])
+        if not os.path.isdir(sys.argv[2] + "/images"):
+            os.mkdir(sys.argv[2] + "/images")
+        if not os.path.isdir(sys.argv[2] + "/containers"):
+            os.mkdir(sys.argv[2] + "/containers")
+        if not os.path.isdir(sys.argv[2] + "/volumes"):
+            os.mkdir(sys.argv[2] + "/volumes")
+        # The following is some rather hacky list operations so that we
+        # can save images by name:tags instead of by ID
+        images = subprocess.check_output("sudo docker images", shell=True)
+        splitImages = images.split()[7:]  # cut off the headers
+        names = []
+        tags = []
+        for i in range(0, len(splitImages)):
+            # only take the image and its tags
+            if (i % 8 == 0):
+                names.append(splitImages[i])
+                tags.append(splitImages[i+1])
+        for i in range(0, len(names)):
+            print("Saving image {0}:{1}".format(names[i], tags[i]))
+            subprocess.call(
+                "sudo docker save {0}:{1} > {2}/images/{0}-{1}.tar".format(
+                    names[i], tags[i], sys.argv[2]), shell=True)
+        subprocess.call(
+            "sudo tar -zcvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes . > /dev/null".format(sys.argv[2]), shell=True)
+        if os.path.isdir("/var/lib/docker/vfs"):
+            subprocess.call("sudo tar -zcvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs . > /dev/null".format(sys.argv[2]), shell=True)
 
-elif sys.argv[1] == "import":
-    if len(sys.argv) != 3:
-        sys.exit("Please specify a directory and try again")
-    if not os.path.isdir(sys.argv[2]):
-        sys.exit("Specified directory {0} does not exist".format(sys.argv[2]))
-    tarballs = subprocess.check_output("ls {0}/images".format(sys.argv[2]), shell=True)
-    splitTarballs = tarballs.split()
-    for i in splitTarballs:
-        print("Loading image {0}".format(i))
-        subprocess.call("sudo docker load < {0}/images/{1}".format(sys.argv[2], i), shell=True)
-    subprocess.call("sudo tar xzvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes > /dev/null".format(sys.argv[2]), shell=True)
-    if os.path.isdir("/var/lib/docker/vfs"):
-	 subprocess.call("sudo tar -xzvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs > /dev/null".format(sys.argv[2]), shell=True)
-    print("If you created directory {0} solely for the purpose of temporary storage for your files during the docker-migrate import/export process, you may now remove it if you so desire".format(sys.argv[2]))
-
-elif sys.argv[1] == "help":
-    print("""
+    elif sys.argv[1] == "import":
+        if len(sys.argv) != 3:
+            sys.exit("Please specify a directory and try again")
+        if not os.path.isdir(sys.argv[2]):
+            sys.exit("Specified directory {0} does not exist".format(sys.argv[2]))
+        tarballs = subprocess.check_output("ls {0}/images".format(sys.argv[2]), shell=True)
+        splitTarballs = tarballs.split()
+        for i in splitTarballs:
+            print("Loading image {0}".format(i))
+            subprocess.call("sudo docker load < {0}/images/{1}".format(sys.argv[2], i), shell=True)
+        subprocess.call(
+            "sudo tar xzvf {0}/volumes/volumeData.tar.gz -C /var/lib/docker/volumes > /dev/null".format(sys.argv[2]), shell=True)
+        if os.path.isdir("/var/lib/docker/vfs"):
+            subprocess.call(
+                "sudo tar -xzvf {0}/volumes/vfsData.tar.gz -C /var/lib/docker/vfs > /dev/null".format(sys.argv[2]), shell=True)
+        print("If you created directory {0} solely for the purpose of temporary storage for your files during the docker-migrate import/export process, you may now remove it if you so desire".format(sys.argv[2]))
+    elif (sys.argv[1] == "help"):
+        sys.exit("""
 # Docker-Migrate
 
 This script allows the user to easily migrate images, volumes, and
@@ -76,7 +82,8 @@ the specified directory into the new docker instance.
 
 Jenny Ramseyer, 2015
 """)
+    else:
+        print("Please specify an option: 'import', 'export', or 'help' and try again")
 else:
-    print("Please specify an option: 'import' or 'export' and try again")
-
+    print("Please specify an option: 'import', 'export', or 'help' and try again")
     
